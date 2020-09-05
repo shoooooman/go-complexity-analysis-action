7dc91f77f50b6b826b958d385ca4ae898a745c51
shoooooman <shoma216@gmail.com>
2020-09-05 19:22:07 +0900

    fix reviewdog config

--- reviewdog.sh
+++ reviewdog.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-go vet -vettool=$(which complexity) . | reviewdog -efm="%f:%l: %m" -reporter=github-pr-review -level=info
+reviewdog -conf=./.reviewdog.yml -reporter=github-pr-review -level=info
