253903e65615b914b343d35834c6d1d47a501fdc
shoooooman <shoma216@gmail.com>
2020-09-05 20:04:14 +0900

    get .reviewdog.yml by curl

--- reviewdog.sh
+++ reviewdog.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 REVIEWDOG_CONFIG_PATH='https://raw.githubusercontent.com/shoooooman/go-complexity-analysis-action/master/.reviewdog.yml'
-# REVIEWDOG_CONFIG=$(curl $GITHUB_ACTION_PATH)
-reviewdog -conf=$GITHUB_ACTION_PATH -reporter=github-pr-review -level=info
+curl $REVIEWDOG_CONFIG_PATH | cat > .reviewdog.yml
+reviewdog -conf=./.reviewdog.yml -reporter=github-pr-review -level=info
