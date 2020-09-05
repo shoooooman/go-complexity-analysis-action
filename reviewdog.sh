86c80a7da9f2303fa6b87a4d096f4f70609e1318
shoooooman <shoma216@gmail.com>
2020-09-05 20:22:25 +0900

    fix reviewdog.sh

--- reviewdog.sh
+++ reviewdog.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
 
-echo $GITHUB_ACTION_PATH
-REVIEWDOG_CONFIG_PATH="$GITHUB_ACTION_PATH/.reviewdog.yml"
+REVIEWDOG_CONFIG_PATH=https://raw.githubusercontent.com/shoooooman/go-complexity-analysis-action/master/.reviewdog.yml
 curl $REVIEWDOG_CONFIG_PATH | cat > .reviewdog_complexity.yml
 reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-review -level=info
