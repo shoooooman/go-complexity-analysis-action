52fde51bccc6f30d0e99b4e042e3ad1a44139100
shoooooman <shoma216@gmail.com>
2020-09-06 14:48:12 +0900

    fix reviewdog.sh

--- reviewdog.sh
+++ reviewdog.sh
@@ -5,7 +5,7 @@ cd $GITHUB_WORKSPACE/$INPUT_WORKDIR || exit 1
 export REVIEWDOG_GITHUB_API_TOKEN="$INPUT_GITHUB_TOKEN"
 
 echo "::group:: reviewdog: go vet -vettool=\$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER $INPUT_WORKDIR..."
-go vet -vettool=\$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER ./... \
+go vet -vettool=$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER ./... \
     | reviewdog \
       -efm="%f:%l: %m" \
       -level=info \
