5bba154fac828d0aa4638dfec10952f804f9bd11
shoooooman <shoma216@gmail.com>
2020-09-06 13:28:13 +0900

    add workdir to inputs

--- reviewdog.sh
+++ reviewdog.sh
@@ -3,12 +3,14 @@
 REVIEWDOG_YML=$(cat << EOS
 runner:
   govet:
-    cmd: go vet -vettool=\$(which complexity) --cycloover=$CYCLOOVER --maintunder=$MAINTUNDER ./...
+    cmd: go vet -vettool=\$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER $INPUT_WORKDIR
     errorformat:
       - "%f:%l: %m"
     level: info
 EOS
 )
 echo "$REVIEWDOG_YML" > .reviewdog_complexity.yml
+
+export REVIEWDOG_GITHUB_API_TOKEN="$INPUT_GITHUB_TOKEN"
 
 reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-review -level=info
