56dcab744fd2520ad30f11fc2b1314b7b2a7ba62
shoooooman <shoma216@gmail.com>
2020-09-06 21:39:42 +0900

    add testdata

---
+++ testdata/a.go
@@ -0,0 +1,28 @@
+package testdata
+
+func f1() {
+	if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else if false {
+	} else {
+	}
+}
+
+func f2() {
+	n := 0
+	switch n {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	case 5:
+	}
+}
