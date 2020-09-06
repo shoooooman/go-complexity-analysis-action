b581ffb8eda5877637efaac86e31eb28b15dc4fa
shoooooman <shoma216@gmail.com>
2020-09-06 15:19:45 +0900

    fix reviewdog.sh

--- reviewdog.sh
+++ reviewdog.sh
@@ -8,7 +8,6 @@ runner:
     cmd: go vet -vettool=\$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER ./...
     errorformat:
       - "%f:%l: %m"
-    level: info
 EOS
 )
 echo "$REVIEWDOG_YML" > .reviewdog_complexity.yml
@@ -16,5 +15,5 @@ echo "$REVIEWDOG_YML" > .reviewdog_complexity.yml
 export REVIEWDOG_GITHUB_API_TOKEN="$INPUT_GITHUB_TOKEN"
 
 echo "::group:: reviewdog: go vet -vettool=\$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER $INPUT_WORKDIR..."
-reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-review -filter-mode=nofilter
+reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-review -level=info -filter-mode=nofilter
 echo '::endgroup::'
