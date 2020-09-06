361d5945437c8184b981d909b5212f08eb7cb9f7
shoooooman <shoma216@gmail.com>
2020-09-06 15:13:52 +0900

    add filter-mode flag

--- reviewdog.sh
+++ reviewdog.sh
@@ -16,5 +16,5 @@ echo "$REVIEWDOG_YML" > .reviewdog_complexity.yml
 export REVIEWDOG_GITHUB_API_TOKEN="$INPUT_GITHUB_TOKEN"
 
 echo "::group:: reviewdog: go vet -vettool=\$(which complexity) --cycloover=$INPUT_CYCLOOVER --maintunder=$INPUT_MAINTUNDER $INPUT_WORKDIR..."
-reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-review -level=info
+reviewdog -conf=./.reviewdog_complexity.yml -reporter=github-pr-review -filter-mode=nofilter
 echo '::endgroup::'
